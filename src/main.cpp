7e1c65ccffd1cd7266b097e288d1e78257b16f12
MR <github2@mailmr.de>
2023-12-14 00:08:25 +0100

    Läuft jetzt

--- src/main.cpp
+++ src/main.cpp
@@ -5,14 +5,17 @@
 #include <DallasTemperature.h>
 #include <PubSubClient.h>
 
-// Deklaration der Funktionen
+// *************** Deklaration der Funktionen
 void connectToWiFi();
 void connectToMQTT();
 void sendTemperatureToMQTT();
 void printSensorAddresses();
 void setup();
 void printWiFiStatus();
+String getTemperatureAsHtml();
+String deviceAddrToStr(DeviceAddress addr);
 
+// *************** Konfig
 
 // WLAN-Zugangsdaten
 char ssid[] = "Muspelheim";
@@ -21,26 +24,63 @@ char pass[] = "dRN4wlan5309GTD9fR";
 // MQTT-Zugangsdaten
 const char *mqttServer = "192.168.66.21";
 const int mqttPort = 1883;
+const char *mqttName = "ArduinoClient";
 const char *mqttUser = "ArudinoNano";
 const char *mqttPassword = "DEIN_MQTT_PASSWORT";
 
 // Pin, an dem der 1-Wire-Bus angeschlossen ist
-const int PinOneWireBus = 2; // Ändere dies entsprechend deiner Verkabelung
+const int PinOneWireBus = 2; // = D2
+
+// ***************  Globale Variablen
+// 1-Wire
 byte addrArray[8];
 OneWire oneWire(PinOneWireBus);
 DallasTemperature sensors(&oneWire);
 
-// Erstelle einen Webserver auf Port 80
+// Webserver
 IPAddress   ip; 
 WiFiServer server(80);
 
-// Erstelle einen MQTT-Client
+// MQTT-Client
 WiFiClient wifiClient;
 PubSubClient mqttClient(wifiClient);
+
+
+// ***************  Funktionen
+
+
+String deviceAddrToStr(DeviceAddress addr) {
+  String returnString = "";
+    for (uint8_t j = 0; j < 8; j++) {
+      if (addr[j] < 16) returnString = "0";
+      returnString = returnString + String(addr[j], HEX);
+    }
+  returnString.toUpperCase();
+  return returnString;
+}
+
+String getTemperatureAsHtml() {
+  Serial.println("getTemperatureAsHtml() begin");
+  String address;
+  String temp;
+  DeviceAddress addr;
+  String returnString = "";
+  for (int i = 0; i < sensors.getDeviceCount(); i++) {
+    float tempC = sensors.getTempCByIndex(i);
+    sensors.getAddress(addr, i);
+    address = deviceAddrToStr(addr);
+    temp = String(tempC);
+    returnString = returnString + address.substring(0, address.length()-2) + "-<b>" + address.substring(address.length()-2) + "</b>: " + temp + " &#8451;</br>";
+    Serial.println("Ermittle Inhalt für Webserver: " + returnString);
+  }
+  return returnString;
+  Serial.println("getTemperatureAsHtml() end");
+}
 
 void setup() {
   // Starte die serielle Kommunikation
   Serial.begin(9600);
+  Serial.println("setup() begin");
 
   // Verbinde mit dem WLAN
   connectToWiFi();
@@ -62,9 +102,11 @@ void setup() {
 
   Serial.println("Gefundene 1-Wire-Sensoren:");
   printSensorAddresses();
+  Serial.println("setup() end");
 }
 
 void printWiFiStatus() {
+  Serial.println("printWiFiStatus() begin");
   // print the SSID of the network you're attached to:
   Serial.print("SSID: ");
   Serial.println(WiFi.SSID());
@@ -77,6 +119,76 @@ void printWiFiStatus() {
   // print where to go in a browser:
   Serial.print("To see this page in action, open a browser to http://");
   Serial.println(ip);
+  Serial.println("printWiFiStatus() end");
+}
+
+void connectToWiFi() {
+  Serial.println("connectToWiFi() begin");
+  Serial.println("Verbinde mit WLAN...");
+  while (WiFi.begin(ssid, pass) != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Verbindung wird hergestellt...");
+  }
+  Serial.println("Verbunden mit WLAN");
+
+  // Starte den Webserver
+  server.begin();
+
+  printWiFiStatus();
+  Serial.println("connectToWiFi() end");
+}
+
+void connectToMQTT() {
+  Serial.println("Verbinde mit MQTT-Server...");
+  mqttClient.setServer(mqttServer, mqttPort);
+  while (!mqttClient.connected()) {
+    if (mqttClient.connect(mqttName, mqttUser, mqttPassword)) {
+      Serial.println("Verbunden mit MQTT-Server");
+    } else {
+      Serial.print("Fehlgeschlagen, rc=");
+      Serial.print(mqttClient.state());
+      Serial.println(" Nächster Versuch in 5 Sekunden...");
+      delay(5000);
+    }
+  }
+}
+
+void sendTemperatureToMQTT() {
+  Serial.println("sendTemperatureToMQTT() begin");
+  String topic = "n/a";
+  String payload;
+  DeviceAddress addr;
+  for (int i = 0; i < sensors.getDeviceCount(); i++) {
+    Serial.println("Ermittle temperatur sensor " + String(i));
+    float tempC = sensors.getTempCByIndex(i);
+    Serial.println("Ermittle adresse sensor " + String(i));
+    sensors.getAddress(addr, i); 
+    topic = "sensor/" + deviceAddrToStr(addr) + "/temperature";
+    payload = String(tempC);
+    Serial.println("topic: " + topic + " - payload: " + payload);
+    mqttClient.publish(topic.c_str(), payload.c_str());
+  }
+  Serial.println("sendTemperatureToMQTT() end");
+}
+
+void printSensorAddresses() {
+  Serial.print("Anzahl: ");
+  Serial.println(sensors.getDeviceCount());
+  for (int i = 0; i < sensors.getDeviceCount(); i++) {
+    DeviceAddress tempAddress;
+    sensors.getAddress(tempAddress, i);
+
+    Serial.print("Sensor ");
+    Serial.print(i + 1);
+    Serial.print(" Adresse: ");
+
+    for (uint8_t j = 0; j < 8; j++) {
+      if (tempAddress[j] < 16) Serial.print("0");
+      Serial.print(tempAddress[j], HEX);
+    }
+
+    Serial.println();
+  }
 }
 
 void loop() {
@@ -109,11 +221,12 @@ void loop() {
             client.print("/\"/>");
             client.print("</head>");
             client.print("<p style=\"font-size:80px; font-family: monospace\">"); 
-            client.print("Sensoren: ");
-            client.print("s <b><a href=\"/m\">+</a>&nbsp;&nbsp;<a href=\"/l\">-</a></b>");
+            client.print("Sensoren: </br>");
+            client.print(getTemperatureAsHtml());
+            //client.print("s <b><a href=\"/m\">+</a>&nbsp;&nbsp;<a href=\"/l\">-</a></b>");
             client.print("<br/>");
-            client.print("<a href=\"/1\">Start</a><br/>");
-            client.print("<a href=\"/0\">Stop</a><br/>");
+            //client.print("<a href=\"/1\">Start</a><br/>");
+            //client.print("<a href=\"/0\">Stop</a><br/>");
             client.print("</p>");
             client.print("</html>");
 
@@ -164,64 +277,5 @@ void loop() {
   sendTemperatureToMQTT();
 
   // Verzögerung
-  delay(1000);
-}
-
-void connectToWiFi() {
-  Serial.println("Verbinde mit WLAN...");
-  while (WiFi.begin(ssid, pass) != WL_CONNECTED) {
-    delay(1000);
-    Serial.println("Verbindung wird hergestellt...");
-  }
-  Serial.println("Verbunden mit WLAN");
-
-  // Starte den Webserver
-  server.begin();
-
-  printWiFiStatus();
-}
-
-void connectToMQTT() {
-  Serial.println("Verbinde mit MQTT-Server...");
-  mqttClient.setServer(mqttServer, mqttPort);
-  while (!mqttClient.connected()) {
-    if (mqttClient.connect("ArduinoClient", mqttUser, mqttPassword)) {
-      Serial.println("Verbunden mit MQTT-Server");
-    } else {
-      Serial.print("Fehlgeschlagen, rc=");
-      Serial.print(mqttClient.state());
-      Serial.println(" Nächster Versuch in 5 Sekunden...");
-      delay(5000);
-    }
-  }
-}
-
-void sendTemperatureToMQTT() {
-  for (int i = 0; i < sensors.getDeviceCount(); i++) {
-    float tempC = sensors.getTempCByIndex(i);
-    String topic = "sensor/" + String(i + 1) + "/temperature";
-    String payload = String(tempC);
-
-    mqttClient.publish(topic.c_str(), payload.c_str());
-  }
-}
-
-void printSensorAddresses() {
-  Serial.print("Anzahl: ");
-  Serial.println(sensors.getDeviceCount());
-  for (int i = 0; i < sensors.getDeviceCount(); i++) {
-    DeviceAddress tempAddress;
-    sensors.getAddress(tempAddress, i);
-
-    Serial.print("Sensor ");
-    Serial.print(i + 1);
-    Serial.print(" Adresse: ");
-
-    for (uint8_t j = 0; j < 8; j++) {
-      if (tempAddress[j] < 16) Serial.print("0");
-      Serial.print(tempAddress[j], HEX);
-    }
-
-    Serial.println();
-  }
+  delay(5000);
 }
